6084ed589c67f9752648416baf1b2a2762136504
CzarCx <garcia.bautista.cesaremiliano@gmail.com>
2023-11-01 11:01:23 -0600

    archivo .sql

--- server/.sql
+++ server/.sql
@@ -1,9 +1,9 @@
-create database mern_stack;
-use mern_stack;
+create database crud_node;
+use crud_node;
 
-create table users (
+create table books (
     id int primary key auto_increment,
-    name char(20) not null,
-    email char(50) not null,
-    password char(70) not null,
+    name char(30) not null,
+    number_pages int not null,
+    name_creator char(30) not null
 );
